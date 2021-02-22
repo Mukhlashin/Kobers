46834b0c4eebefddb6822726f2b454f2d0f39867
SaeMaulidin <sadammaulidin@gmail.com>
2021-02-22 10:20:07 +0700

    xml login & add font

--- app/src/main/java/com/example/kobers/activities/login/LoginActivity.kt
+++ app/src/main/java/com/example/kobers/activities/login/LoginActivity.kt
@@ -2,11 +2,15 @@ package com.example.kobers.activities.login
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.TypedValue
+import androidx.core.content.res.ResourcesCompat
 import com.example.kobers.R
+import kotlinx.android.synthetic.main.activity_login.*
 
 class LoginActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
+
     }
 }
