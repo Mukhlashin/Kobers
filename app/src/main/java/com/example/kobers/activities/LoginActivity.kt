eb818bbd4651b0ce77815837c9a0cf266b2c939a
Abdullah Mukhlashin <abdullahmukhlashiin@gmail.com>
2021-02-25 14:42:45 +0800

    adding login logic, but its still need to be fixed, also adding Constants.kt and Model.kt

--- app/src/main/java/com/example/kobers/activities/LoginActivity.kt
+++ app/src/main/java/com/example/kobers/activities/LoginActivity.kt
@@ -1,8 +1,10 @@
 package com.example.kobers.activities
 
+import android.annotation.SuppressLint
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.text.Editable
 import android.text.TextWatcher
 import android.util.TypedValue
 import android.view.View
@@ -13,6 +15,7 @@ import androidx.core.content.res.ResourcesCompat
 import com.example.kobers.R
 import com.example.kobers.activities.HomeActivity
 import com.example.kobers.activities.RegisterActivity
+import com.google.android.material.textfield.TextInputLayout
 import com.google.firebase.auth.FirebaseAuth
 import kotlinx.android.synthetic.main.activity_login.*
 
@@ -26,12 +29,88 @@ class LoginActivity : AppCompatActivity() {
             finish() }
     }
 
+    @SuppressLint("ClickableViewAccessibility")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         requestWindowFeature(Window.FEATURE_NO_TITLE)
         setContentView(R.layout.activity_login)
 
+        setTextChangedListener(nama_toko_login, til_nama_toko_login)
+        setTextChangedListener(password_login, til_password_login)
+        progress_layout.setOnTouchListener { _ , _ -> true }
 
+        login_button.setOnClickListener {
+            onLogin()
+        }
+
+        txt_signup.setOnClickListener {
+            onSignup()
+        }
+    }
+
+    private fun setTextChangedListener(edt: EditText, til: TextInputLayout) {
+        edt.addTextChangedListener(object : TextWatcher {
+            override fun afterTextChanged(s: Editable?) {
+
+            }
+            override fun beforeTextChanged(s: CharSequence? , start: Int, count: Int, after: Int) {
+
+            }
+            // ketika editText diubah memastikan TextInputLayout tidak menunjukkan pesan error
+            override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {
+                til.isErrorEnabled = false
+            }
+        })
+    }
+
+    private fun onLogin() {
+        var proceed = true
+        if (nama_toko_login.text.isNullOrEmpty()) {
+            til_nama_toko_login.error = "Required Email"
+            til_nama_toko_login.isErrorEnabled = true
+            proceed = false
+        }
+        if (password_login.text.isNullOrEmpty()) {
+            til_password_login.error = "Required Password"
+            til_password_login.isErrorEnabled = true
+            proceed = false
+        }
+
+        if (proceed) {
+            progress_layout.visibility = View.VISIBLE
+            firebaseAuth.signInWithEmailAndPassword(
+                nama_toko_login.text.toString(),
+                password_login.text.toString()
+            )
+                .addOnCompleteListener { task ->
+                    if (!task.isSuccessful) {
+                        progress_layout.visibility = View.GONE
+                        Toast.makeText(
+                            this@LoginActivity,
+                            "Login error: ${task.exception?.localizedMessage}",
+                            Toast.LENGTH_SHORT).show()
+                    }
+                }
+                .addOnFailureListener { e ->
+                    progress_layout.visibility = View.GONE
+                    e.printStackTrace()
+                }
+        }
+    }
+
+    private fun onSignup() {
+        startActivity(Intent(this, RegisterActivity::class.java))
+        finish()
+    }
+
+    override fun onStart() {
+        super.onStart() // method yang pertama kali dijalankan sebelum method lainnya
+        firebaseAuth.addAuthStateListener(firebaseAuthListener)
+    }
+
+    override fun onStop() {
+        super.onStop() // dijalankan jika proses dalam activity selesai atau dihentikan system
+        firebaseAuth.removeAuthStateListener(firebaseAuthListener)
     }
 
 //    private fun onLogin() {
@@ -68,7 +147,7 @@ class LoginActivity : AppCompatActivity() {
 //                }
 //        }
 //    }
-
+//
 //    private fun onSignup() {
 //        startActivity(Intent(this, RegisterActivity::class.java))
 //        finish()
--- app/src/main/java/com/example/kobers/activities/RegisterActivity.kt
+++ app/src/main/java/com/example/kobers/activities/RegisterActivity.kt
@@ -1,12 +1,126 @@
 package com.example.kobers.activities
 
+import android.annotation.SuppressLint
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.text.Editable
+import android.text.TextWatcher
+import android.view.View
+import android.widget.EditText
+import android.widget.Toast
 import com.example.kobers.R
+import com.example.kobers.utils.Constants.DATA_USERS
+import com.example.kobers.utils.User
+import com.google.android.material.textfield.TextInputLayout
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.firestore.FirebaseFirestore
+import kotlinx.android.synthetic.main.activity_register.*
 
 class RegisterActivity : AppCompatActivity() {
+
+    val firebaseDb = FirebaseFirestore.getInstance()
+    val firebaseAuth = FirebaseAuth.getInstance()
+    val firebaseAuthListener = FirebaseAuth.AuthStateListener {
+        val user = firebaseAuth.currentUser?.uid
+        if (user != null) {
+            startActivity(Intent(this, HomeActivity::class.java))
+            finish()
+        }
+    }
+
+    @SuppressLint("ClickableViewAccessibility")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_register)
+
+        setTextChangedListener(nama_toko_regis, til_nama_toko_regis)
+        setTextChangedListener(nomor_regis, til_nomor_regis)
+        setTextChangedListener(password_regis, til_password_regis)
+        progress_layout.setOnTouchListener { _, _ -> true }
+        btn_register.setOnClickListener {
+            onSignup()
+        }
+        txt_login.setOnClickListener {
+            onLogin()
+        }
+    }
+
+    private fun setTextChangedListener(edt: EditText, til: TextInputLayout) { edt.addTextChangedListener(object :
+        TextWatcher {
+        override fun afterTextChanged(s: Editable?) { }
+
+        override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) { }
+
+        override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) { til.isErrorEnabled = false }
+    })
+    }
+
+    private fun onLogin() {
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    }
+
+    override fun onStart() {
+        super.onStart()
+        firebaseAuth.addAuthStateListener(firebaseAuthListener)
+    }
+
+    override fun onStop() {
+        super.onStop()
+        firebaseAuth.removeAuthStateListener(firebaseAuthListener)
+    }
+
+    private fun onSignup() {
+        var proceed = true
+        if (nama_toko_regis.text.isNullOrEmpty()) {
+            til_nama_toko_regis.error = "Required Name"
+            til_nama_toko_regis.isErrorEnabled = true
+            proceed = false
+        }
+        if (nomor_regis.text.isNullOrEmpty()) {
+            til_nomor_regis.error = "Required Phone Number"
+            til_nomor_regis.isErrorEnabled = true
+            proceed = false
+        }
+        if (password_regis.text.isNullOrEmpty()) {
+            til_password_regis.error = "Required Password"
+            til_password_regis.isErrorEnabled = true
+            proceed = false
+        }
+
+        if (proceed) {
+            progress_layout.visibility = View.VISIBLE
+            firebaseAuth.createUserWithEmailAndPassword(
+                nomor_regis.text.toString(),
+                password_regis.text.toString()
+            )
+                .addOnCompleteListener { task ->
+                    if (!task.isSuccessful) {
+                        progress_layout.visibility = View.GONE
+                        Toast.makeText(
+                            this@RegisterActivity,
+                            "SignUp error: ${task.exception?.localizedMessage}",
+                            Toast.LENGTH_SHORT
+                        ).show()
+                    } else if (firebaseAuth.uid != null) {
+                        val namaToko = nama_toko_regis.text.toString()
+                        val phone = nomor_regis.text.toString()
+                        val password = password_regis.text.toString()
+                        val user = User(
+                            namaToko,
+                            phone,
+                            password
+                        )
+                        firebaseDb.collection(DATA_USERS)
+                            .document(firebaseAuth.uid!!).set(user)
+                    }
+                    progress_layout.visibility = View.GONE
+                }
+
+                .addOnFailureListener {
+                    progress_layout.visibility = View.GONE
+                    it.printStackTrace()
+                }
+        }
     }
 }
